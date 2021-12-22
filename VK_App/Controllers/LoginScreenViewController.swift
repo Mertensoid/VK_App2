528fb308314491c4ec3fb108fae049551d569e49
Mertensoid <matsoukm@yandex.ru>
2021-12-22 19:38:08 +0300

    Homework-2-1

---
+++ VK_App/Controllers/LoginScreenViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  VK_App
+//
+//  Created by admin on 19.12.2021.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginScreenScrollView: UIScrollView!
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        if usernameTextField.text == "admin" && passwordTextField.text == "123" {
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginScreenScrollView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+           
+        // Подписываемся на два уведомления: одно приходит при появлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+        // Второе — когда она пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+       }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    // Когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+           
+        // Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.loginScreenScrollView?.contentInset = contentInsets
+           loginScreenScrollView?.scrollIndicatorInsets = contentInsets
+        UIView.animate(withDuration: 1) {
+            self.loginScreenScrollView.constraints
+                .first(where: { $0.identifier == "keyboardShown" })?
+                .priority = .required
+            self.loginScreenScrollView.constraints
+                .first(where: { $0.identifier == "keyboardHide" })?
+                .priority = .defaultHigh
+            self.view.layoutIfNeeded()
+        }
+    }
+       
+    //Когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        // Устанавливаем отступ внизу UIScrollView, равный 0
+        let contentInsets = UIEdgeInsets.zero
+        loginScreenScrollView?.contentInset = contentInsets
+        UIView.animate(withDuration: 1) {
+            self.loginScreenScrollView.constraints
+                .first(where: { $0.identifier == "keyboardShown" })?
+                .priority = .defaultHigh
+            self.loginScreenScrollView.constraints
+                .first(where: { $0.identifier == "keyboardHide" })?
+                .priority = .required
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func hideKeyboard() {
+        self.loginScreenScrollView?.endEditing(true)
+    }
+}
+
