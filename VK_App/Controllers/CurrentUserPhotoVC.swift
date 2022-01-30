0c5b4e455c620fb702f36d39fa7412418cd00e8d
Mertensoid <matsoukm@yandex.ru>
2022-01-31 00:22:43 +0300

    Задание 1 ДЗ №8 выполнено

--- VK_App/Controllers/CurrentUserPhotoVC.swift
+++ VK_App/Controllers/CurrentUserPhotoVC.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class CurrentUserPhotoVC: UIViewController {
     @IBOutlet weak var currentImageView: UIImageView!
+    //@IBOutlet weak var leftImageView: UIImageView!
+    //@IBOutlet weak var rightImageView: UIImageView!
     
-    let leftImageView = UIImageView()
-    let rightImageView = UIImageView()
+    var leftImageView = UIImageView()
+    var rightImageView = UIImageView()
     
     var photos: [UIImage] = []
     var currentPhotoIndex: Int = 0
@@ -20,10 +22,9 @@ class CurrentUserPhotoVC: UIViewController {
     
     private var isAnimated = false
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    private func setNewPhotos() {
         self.currentImageView.image = photos[currentPhotoIndex]
+
         if currentPhotoIndex == 0 && currentPhotoIndex != photos.count-1 {
             leftImageView.image = photos[photos.count-1]
             rightImageView.image = photos[currentPhotoIndex + 1]
@@ -37,53 +38,78 @@ class CurrentUserPhotoVC: UIViewController {
             leftImageView.image = photos[currentPhotoIndex - 1]
             rightImageView.image = photos[currentPhotoIndex + 1]
         }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setNewPhotos()
         
         leftImageView.frame.size.height = currentImageView.frame.height
-        leftImageView.frame.size.width = currentImageView.frame.width
-        leftImageView.center.x = currentImageView.center.x - currentImageView.frame.width
-        leftImageView.center.y = currentImageView.center.y
+        leftImageView.frame.size.width = view.bounds.width
+        leftImageView.center.x = currentImageView.center.x - currentImageView.bounds.width
+        leftImageView.center.y = view.center.y
         rightImageView.frame.size.height = currentImageView.frame.height
-        rightImageView.frame.size.width = currentImageView.frame.width
-        rightImageView.center.x = currentImageView.center.x + currentImageView.frame.width
-        rightImageView.center.y = currentImageView.center.y
-        
-        self.view.addSubview(leftImageView)
-        self.view.addSubview(rightImageView)
+        rightImageView.frame.size.width = view.bounds.width
+        rightImageView.center.x = currentImageView.center.x + currentImageView.bounds.width
+        rightImageView.center.y = view.center.y
+
+        leftImageView.contentMode = .scaleAspectFit
+        rightImageView.contentMode = .scaleAspectFit
+
+        currentImageView.addSubview(leftImageView)
+        currentImageView.addSubview(rightImageView)
         
         
         currentImageView.isUserInteractionEnabled = true
-        leftImageView.isUserInteractionEnabled = true
-        rightImageView.isUserInteractionEnabled = true
         
-        currentImageView.addGestureRecognizer(UISwipeGestureRecognizer(
-            target: self,
-            action: #selector(didSwipe(_:))))
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe(_:)))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe(_:)))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
     }
     
     @objc
     private func didSwipe(_ gesture: UISwipeGestureRecognizer) {
         switch gesture.direction {
-        case .left:
-            UIView.animate(
-                withDuration: 1,
-                delay: 0,
-                animations: {self.rightImageView.center.x -= self.currentImageView.frame.width},
-                completion: nil)
-//            propertyAnimator = UIViewPropertyAnimator(
-//                duration: 1,
-//                curve: .linear,
-//                animations: {
-//                    self.rightImageView.center.x -= self.currentImageView.frame.width
-//                })
-//            propertyAnimator.pauseAnimation()
         case .right:
+            print("right")
             UIView.animate(
-                withDuration: 1,
+                withDuration: 0.3,
                 delay: 0,
-                animations: {self.leftImageView.center.x += self.currentImageView.frame.width},
-                completion: nil)
-//            let translation = gesture.translation(in: self.rightImageView)
-//            propertyAnimator.fractionComplete = -translation.y
+                animations: {self.leftImageView.center.x = self.currentImageView.center.x},
+                completion: {_ in
+                    if self.currentPhotoIndex == 0 {
+                        self.currentPhotoIndex = self.photos.count-1
+                        print(self.currentPhotoIndex)
+                    } else {
+                        self.currentPhotoIndex -= 1
+                        print(self.currentPhotoIndex)
+                    }
+                    self.setNewPhotos()
+                    self.leftImageView.center.x = self.currentImageView.center.x - self.currentImageView.bounds.width
+                })
+        case .left:
+            print("left")
+            UIView.animate(
+                withDuration: 0.3,
+                delay: 0,
+                animations: {self.rightImageView.center.x = self.currentImageView.center.x},
+                completion: {_ in
+                    switch self.currentPhotoIndex {
+                    case self.photos.count-1:
+                        self.currentPhotoIndex = 0
+                        print(self.currentPhotoIndex)
+                    default:
+                        self.currentPhotoIndex += 1
+                        print(self.currentPhotoIndex)
+                    }
+                    self.setNewPhotos()
+                    self.rightImageView.center.x = self.currentImageView.center.x + self.currentImageView.bounds.width
+            })
         default:
             return
         }
